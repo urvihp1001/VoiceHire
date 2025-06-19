313354d1f74c5837964eed9b752da6ebb3069932
urvihp1001 <urvi.email@gmail.com>
2025-06-19 21:42:33 +0530

    corrected data context

--- app/interview/[interview_id]/start/page.jsx
+++ app/interview/[interview_id]/start/page.jsx
@@ -12,6 +12,14 @@ function StartInterview(){
   const {interviewInfo, setInterviewInfo} = useContext(InterviewDataContext);
   const vapi= new Vapi(process.env.NEXT_PUBLIC_VAPI_PUBLIC_KEY)
 
+  useEffect(()=>{
+    interviewInfo && vapi.startInterview();
+  },[interviewInfo])
+  const startCall=()=>{
+    if(interviewInfo){
+
+    }
+  }
   return(
     
       <div className='p-20 lg:px-48 xl:px-56'>
@@ -25,15 +33,25 @@ function StartInterview(){
   </h2>
 
 <div className='grid grid-cols-1 md:grid-cols-2 gap-7 mt-5'>
-  <div className='bg-white h-[400px] rounded-lg border flex items-center justify-center '>
-    <Image src={'/ai.jpg'} alt='ai' width={100} height={100}
-    className='w-[60px] h-[60px] rounded-full object-cover' 
+  <div className='bg-white h-[400px] rounded-lg border flex flex-col gap-3 items-center justify-center '>
+    <Image src={'/ai.jpg'} alt='ai' width={100} height={100} 
+    className='w-[100px] h-[100px] rounded-full object-cover' 
     />
-
+    <h2>AI Recruiter</h2>
+</div>
+ <div className='bg-white h-[400px] rounded-lg border flex flex-col gap-3 items-center justify-center '>
+   <h2
+   className='text-7xl font-bold text-center bg-primary text-white p-3 rounded-full px-5'
+   >{interviewInfo?.username[0]}</h2>
+<h2>{interviewInfo?.username}</h2>
 </div>
 </div>
+<div className='flex justify-center items-center mt-10 gap-5'>
+  <Mic className='bg-gray-500 p-3 rounded-full text-white w-12 h-12  cursor-pointer' onClick={()=>{}} />
+  <PhoneOff className='bg-red-500 text-white p-3 rounded-full w-12 h-12 ml-3 cursor-pointer' onClick={()=>{}} />
 </div>
-
+<h2 className='text-sm text-gray-400 text-center mt-5'>Interview in Progress...</h2>
+</div>
     
 
 
--- app/interview/layout.jsx
+++ app/interview/layout.jsx
@@ -5,7 +5,7 @@ import { InterviewDataContext } from '@/app/context/InterviewDataContext';
 function InterviewLayout({ children }) {
   const [interviewInfo, setInterviewInfo] = React.useState();
   return (
-    <InterviewDataContext.Provider value={[interviewInfo, setInterviewInfo]}>
+    <InterviewDataContext.Provider value={{interviewInfo, setInterviewInfo}}>
       <div className='bg-secondary h-screen'>
         <InterviewHeader/>
         {children}
